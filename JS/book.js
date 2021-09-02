95b4211543ca03dc1b2ec784c5124a05abae37fe
bakhtiarabid <abid.bu16@gmail.com>
2021-09-02 17:44:47 +0600

    Showing Results Added

--- JS/book.js
+++ JS/book.js
@@ -1,6 +1,7 @@
 const searchField = document.getElementById("search-field");
 const bookInfoContainer = document.getElementById("book-info");
 const errorMessage = document.getElementById("error-message");
+const countingResults = document.getElementById("result-count");
 
 /* ------------------------
 Search Book
@@ -17,7 +18,7 @@ const searchBook = () => {
    const url = `https://openlibrary.org/search.json?q=${searchText}`;
    fetch(url)
       .then((res) => res.json())
-      .then((data) => displayBookInfo(data.docs));
+      .then((data) => displayBookInfo(data));
 };
 
 /* ------------------------
@@ -26,9 +27,23 @@ Display Search Result
 
 const displayBookInfo = (book) => {
    console.log(book);
+   bookInfoContainer.textContent = "";
+   const books = book.docs;
+
+   console.log(book);
+   /* Showing Resutls */
+   countingResults.innerHTML = `
+   <div class="bg-success text-center text-white p-5 rounded-3 py-5">
+   <h3 >Total Number Found: ${book.numFound}</h3>
+   <h3 >Total Number of Books: ${books.length}</h3>
+   </div>
+   
+
+   
+   `;
 
    /* Error Handling */
-   if (book.length === 0) {
+   if (books.length === 0) {
       const div = document.createElement("div");
       div.innerHTML = `
       <h3 class="bg-danger text-center text-white p-5 rounded-3">
@@ -37,33 +52,39 @@ const displayBookInfo = (book) => {
       `;
       errorMessage.appendChild(div);
    }
-   bookInfoContainer.textContent = "";
 
-   book.forEach((books) => {
+   countingResults.classList.add("d-block");
+   books.forEach((item) => {
       const imgUrl = `https://covers.openlibrary.org/b/id/${
-         books.cover_i ? books.cover_i : "10909258"
+         item.cover_i ? item.cover_i : "10909258"
       }-M.jpg`;
 
-      console.log(books);
-
-      if (
-         books.title !== undefined &&
-         books.author_name !== undefined &&
-         books.first_publish_year !== undefined
-      ) {
-         const div = document.createElement("div");
-         div.classList.add("col");
-         div.innerHTML = `
+      // const publisherName = books.publisher;
+      // console.log(books.publisher);
+      searchField.value = "";
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
                 <div class="card h-100">
                      <img src="${imgUrl}" class="card-img-top" alt="..." />
                      <div class="card-body">
-                        <h5 class="card-title">Book Title: ${books.title}</h5>
+                        <h5 class="card-title">Book Title: ${
+                           item.title ? item.title : "N/A"
+                        }</h5>
                         <p class="card-text">
-                           <span class="fw-bold">Author Name: </span>${books.author_name}
+                           <span class="fw-bold">Author Name: </span>${
+                              item.author_name ? item.author_name[0] : "N/A"
+                           }
                            <br/>
-                           <span class="fw-bold" >Publisher: </span>${books.publisher}
+                           <span class="fw-bold" >Publisher: </span>${
+                              item.publisher ? item.publisher[0] : "N/A"
+                           }
                            <br/>
-                           <span class="fw-bold" >First Publish Year: </span>${books.first_publish_year}
+                           <span class="fw-bold" >First Publish Year: </span>${
+                              item.first_publish_year
+                                 ? item.first_publish_year
+                                 : "N/A"
+                           }
                         </p>
                      </div>
                      
@@ -71,11 +92,6 @@ const displayBookInfo = (book) => {
     
     
     `;
-         bookInfoContainer.appendChild(div);
-      }
+      bookInfoContainer.appendChild(div);
    });
 };
-
-/*  books.author_name.forEach((authors) => {
-    console.log(authors);
- }); */
