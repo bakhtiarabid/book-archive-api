b66887c8a1ced1f18793ec82f2daf2094cbfbdea
bakhtiarabid <abid.bu16@gmail.com>
2021-09-02 12:55:41 +0600

    Error Handling Added

--- JS/book.js
+++ JS/book.js
@@ -1,10 +1,15 @@
+const searchField = document.getElementById("search-field");
+const bookInfoContainer = document.getElementById("book-info");
+const errorMessage = document.getElementById("error-message");
+
 /* ------------------------
 Search Book
 --------------------------- */
 
 const searchBook = () => {
-   const searchField = document.getElementById("search-field");
    const searchText = searchField.value;
+   searchField.value = "";
+   errorMessage.textContent = "";
 
    //    console.log(searchFieldText);
 
@@ -20,35 +25,54 @@ Display Search Result
 --------------------------- */
 
 const displayBookInfo = (book) => {
-   const bookInfoContainer = document.getElementById("book-info");
+   console.log(book);
 
+   /* Error Handling */
+   if (book.length === 0) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+      <h3 class="bg-danger text-center text-white p-5 rounded-3">
+               Please Input a valid name
+            </h3>
+      `;
+      errorMessage.appendChild(div);
+   }
    bookInfoContainer.textContent = "";
+
    book.forEach((books) => {
-      const imgUrl = `https://covers.openlibrary.org/b/id/${books.cover_i}-L.jpg`;
-      const noImgUrl = `https://covers.openlibrary.org/b/id/undefined-L.jpg`;
+      const imgUrl = `https://covers.openlibrary.org/b/id/${
+         books.cover_i ? books.cover_i : "10909258"
+      }-M.jpg`;
 
       console.log(books);
-      const div = document.createElement("div");
-      div.classList.add("col");
-      div.innerHTML = `
+
+      if (
+         books.title !== undefined &&
+         books.author_name !== undefined &&
+         books.first_publish_year !== undefined
+      ) {
+         const div = document.createElement("div");
+         div.classList.add("col");
+         div.innerHTML = `
                 <div class="card h-100">
                      <img src="${imgUrl}" class="card-img-top" alt="..." />
                      <div class="card-body">
                         <h5 class="card-title">Book Title: ${books.title}</h5>
                         <p class="card-text">
-                           Author Name: 
+                           <span class="fw-bold">Author Name: </span>${books.author_name}
+                           <br/>
+                           <span class="fw-bold" >Publisher: </span>${books.publisher}
+                           <br/>
+                           <span class="fw-bold" >First Publish Year: </span>${books.first_publish_year}
                         </p>
                      </div>
-                     <div class="card-footer">
-                        <small class="text-muted"
-                           >Last updated 3 mins ago</small
-                        >
-                     </div>
+                     
                   </div>
     
     
     `;
-      bookInfoContainer.appendChild(div);
+         bookInfoContainer.appendChild(div);
+      }
    });
 };
 
--- README.md
+++ README.md
@@ -1,22 +1,25 @@
 # book-archive
 
-## [Private Repository Link](https://classroom.github.com/a/mOiiFMDY)
-Please Click this link to create your assignment private repo: [https://classroom.github.com/a/mOiiFMDY](https://classroom.github.com/a/mOiiFMDY)
+## [Private Repository Link](https://github.com/Programming-Hero-Web-Course4/book-archive-bakhtiarabid)
+
+Please Click this link to see live Website: [https://book-archive-api-abid.netlify.app/](BOOK ARCHIVE ASSIGNMENT)
 
 ## API Links
 
 ### Book Search
+
 URL Format: https://openlibrary.org/search.json?q=${searchText}
 
 Example: https://openlibrary.org/search.json?q=javascript
 
-
 ### Book cover image url: Large (user either large or medium image)
+
 URL Format: https://covers.openlibrary.org/b/id/{cover_i}-L.jpg
 
 Example: https://covers.openlibrary.org/b/id/554106-L.jpg
 
 ### Book cover image url: Medium
+
 URL Format: https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
 
 Example: https://covers.openlibrary.org/b/id/554106-M.jpg
